efe2a8b439180754719875e651627211f875d0af
Refren86 <denis1975shvaiber@gmail.com>
2025-03-08 19:54:33 +0200

    Addjusted lint staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 export default {
   "**/*.{ts,tsx}": ["eslint --fix", "prettier --write"],
-  "**/*.{js,jsx,json,css,scss}": ["prettier --write"],
+  "**/*.{js,jsx,css,scss}": ["prettier --write"],
 };
