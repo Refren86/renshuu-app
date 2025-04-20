5c38987c7bb745b9848d38f9fb86ffba21e2bf4e
Denys Shvaiber <55783317+Refren86@users.noreply.github.com>
2025-04-20 23:11:29 +0300

    Dev (#11)
    * dev branch setup
    
    * CI
    
    * Updated node version
    
    * changed actions version
    
    * update package-lock file
    
    * test
    
    * test
    
    * test
    
    * Added ts check
    
    * adjustment
    
    * apply changes
    
    * push
    
    * apply changes
    
    * Tweaks
    
    * file name change and build info remove
    
    * updated husky file
    
    * Adding image upload functionality
    
    * Cloudinary image logic
    
    * Working on image functionality
    
    * Playing with my prey
    
    * Graphql hooks tested
    
    * idk
    
    * test
    
    * test
    
    * test
    
    * meh
    
    * ayaya
    
    * Configured docker for dev with hot reload

--- frontend/src/components/dnd/AddCard.tsx
+++ frontend/src/components/dnd/AddCard.tsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
 import { motion } from "framer-motion";
-import { CopyPlus } from "lucide-react";
+import { useRef, useState } from "react";
+import { CircleX, CopyPlus, Eye, Upload } from "lucide-react";
 
 import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 import { Textarea } from "../ui/textarea";
 import { FlashcardStatus, TFlashcard } from "@/types";
+import { ImagePreviewModal } from "@/containers/VocabularyView/ImagePreviewModal/ImagePreviewModal";
 
 type AddCardProps = {
   column: FlashcardStatus;
@@ -12,16 +14,34 @@ type AddCardProps = {
 };
 
 export const AddCard = ({ column, onCreateFlashcard }: AddCardProps) => {
+  const imgRef = useRef<HTMLInputElement>(null);
+
   const [newCardData, setNewCardData] = useState<Pick<TFlashcard, "kanji" | "reading" | "meaning">>({
     kanji: "",
     reading: "",
     meaning: "",
   });
   const [adding, setAdding] = useState(false);
+  const [imagePreview, setImagePreview] = useState<string | null>(null);
+  const [, setImageFile] = useState<File | null>(null);
+  const [showImagePreview, setShowImagePreview] = useState(false);
 
   function handleInputChange(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
     const { name, value } = e.target;
     setNewCardData((prevState) => ({ ...prevState, [name]: value }));
+  }
+
+  function handleImageChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const { files } = e.target;
+
+    if (files && files.length > 0) {
+      const file = files[0];
+
+      const imagePreviewURL = URL.createObjectURL(file);
+
+      setImageFile(file);
+      setImagePreview(imagePreviewURL);
+    }
   }
 
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
@@ -46,36 +66,54 @@ export const AddCard = ({ column, onCreateFlashcard }: AddCardProps) => {
     setAdding(false);
   }
 
+  function toggleForm() {
+    setAdding((prev) => !prev);
+    setImagePreview(null);
+    setImageFile(null);
+  }
+
+  function toggleImagePreview() {
+    setShowImagePreview((prev) => !prev);
+  }
+
   return (
     <>
+      <ImagePreviewModal isOpen={showImagePreview} onClose={toggleImagePreview} imageSrc={imagePreview} />
       {adding ? (
         <motion.form layout onSubmit={handleSubmit}>
           <div className="space-y-2">
             <Input name={"kanji" as FlashcardStatus} onChange={handleInputChange} autoFocus placeholder="Kanji" />
             <Input name={"reading" as FlashcardStatus} onChange={handleInputChange} placeholder="Reading" />
             <Textarea name={"meaning" as FlashcardStatus} onChange={handleInputChange} placeholder="Meaning" />
+            <Input ref={imgRef} type="file" accept="image/*" className="sr-only" onChange={handleImageChange} />
+            {imagePreview ? (
+              <Button type="button" className="w-full" onClick={toggleImagePreview}>
+                <Eye />
+                <span className="ml-2">Preview image</span>
+              </Button>
+            ) : (
+              <Button type="button" className="w-full" onClick={() => imgRef.current?.click()}>
+                <Upload />
+                <span className="ml-2">Upload image</span>
+              </Button>
+            )}
           </div>
 
-          <div className="mt-1.5 flex items-center justify-end gap-1.5">
-            <button
-              onClick={() => setAdding(false)}
-              className="px-3 py-1.5 text-xs text-red-400 transition-colors hover:text-neutral-50"
-            >
-              Close
-            </button>
-            <button
-              type="submit"
-              className="flex items-center gap-1.5 rounded bg-neutral-50 px-3 py-1.5 text-xs text-neutral-950 transition-colors hover:bg-neutral-300"
-            >
-              <span>Add</span>
+          <div className="mt-2 flex items-center justify-end gap-1.5">
+            <Button onClick={toggleForm} variant="destructive">
+              <CircleX />
+              <span className="ml-2">Close</span>
+            </Button>
+            <Button type="submit" variant="success">
               <CopyPlus />
-            </button>
+              <span className="ml-2">Add</span>
+            </Button>
           </div>
         </motion.form>
       ) : (
         <motion.button
           layout
-          onClick={() => setAdding(true)}
+          onClick={toggleForm}
           className="flex w-full items-center gap-1.5 px-3 py-1.5 text-xs text-neutral-400 transition-colors hover:text-neutral-50"
         >
           <span>Add card</span>
